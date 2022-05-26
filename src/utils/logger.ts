8da813d06abfa8497a79859421103db3f3f815db
Kristiyan Borisov <genkoph@protonmail.com>
2022-05-26 11:03:53 +0300

    feat: add new logging functions

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,18 @@
 import colors from "colors/safe";
 import { PLUGIN_NAME } from "../config/plugin.config";
 
-export function message(message: string) {
-  console.log(`${colors.green(PLUGIN_NAME)}: ${message}`);
+function message(sender: string, messageText: string) {
+  console.log(colors.green(`[${sender}] ${messageText}`));
+}
+
+export function pluginMessage(messageText: string) {
+  message(PLUGIN_NAME, messageText);
+}
+
+export function defaultMessage(messageText: string) {
+  console.log(messageText);
+}
+
+export function clearConsole() {
+  console.clear();
 }
