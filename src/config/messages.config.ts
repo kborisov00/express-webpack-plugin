9499df5bf04c287d6f978a520cece564837ed585
Kristiyan Borisov <genkoph@protonmail.com>
2022-05-26 22:41:34 +0300

    feat: add new messages in messages.config

--- src/config/messages.config.ts
+++ src/config/messages.config.ts
@@ -1,6 +1,10 @@
 const MESSAGES = {
   init: "initializing ... ",
   restarting: "restarting due to changes ...",
+
+  fileNameError:
+    'You need to specify "filename" property in the configuration file.',
+  pathError: 'You need to specify "path" property in the configuration file.',
 };
 
 export default MESSAGES;
